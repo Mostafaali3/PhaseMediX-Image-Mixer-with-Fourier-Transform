38a6da4d4b18cda3c4f76feed4b5e8436b271ca5
Mostafaali3 <mostafaalisarhan@gmail.com>
2024-11-30 21:48:58 +0200

    Create controller.py

---
+++ classes/controller.py
@@ -0,0 +1,14 @@
+class Controller():
+    def __init__(self):
+        self.list_of_images = None
+        self.list_of_image_viewers = None
+        self.list_of_component_viewers = None
+        self.Mixer = None
+        self.result_image_1 = None
+        self.result_image_2 = None
+        
+    def set_current_images_list(self):
+        pass
+    
+    def mix_all(self, output_viewer_number:int):
+        pass

ac2320c392d68e8d3c71635cc47da158f5334174
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2024-11-30 23:17:31 +0200

    added the resize function in customImage

--- classes/customImage.py
+++ classes/customImage.py
@@ -4,16 +4,19 @@ import cv2
 import matplotlib.pyplot as plt
 
 class CustomImage():
-    def __init__(self , image):
-        imported_image_gray_scale = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
-        image_x_components = np.arange(1 , imported_image_gray_scale.shape[0] +1 )
-        image_y_components = np.arange(1 , imported_image_gray_scale.shape[1] +1 )
-        self.__original_image = np.empty((3,), dtype=object)
-        self.__original_image[0] = image_x_components
-        self.__original_image[1] = image_y_components
-        self.__original_image[2] = np.array(imported_image_gray_scale)
-        self.__modified_image = deepcopy(self.__original_image)
-        self.__original_image_fourier_components = np.empty((1,) , dtype= object)
+    def __init__(self , image = None):
+        if (len(image) != 0 ):
+            imported_image_gray_scale = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+            # Height
+            image_x_components = np.arange(1 , imported_image_gray_scale.shape[0] +1 )
+            # Width
+            image_y_components = np.arange(1 , imported_image_gray_scale.shape[1] +1 )
+            self.__original_image = np.empty((3,), dtype=object)
+            self.__original_image[0] = image_x_components
+            self.__original_image[1] = image_y_components
+            self.__original_image[2] = np.array(imported_image_gray_scale)
+            self.__modified_image = deepcopy(self.__original_image)
+            self.__original_image_fourier_components = np.empty((1,) , dtype= object)
         
     @property
     def original_image(self):
@@ -49,8 +52,18 @@ class CustomImage():
         
     
     def fourier_transform_image(self):
-        fourier_transformed_image = np.fft.fft2(self.original_image[2])
-        shifted_fourier_transformed_iamge = np.fft.fftshift(fourier_transformed_image)
-        self.original_image_fourier_components = shifted_fourier_transformed_iamge
+        fourier_transformed_image = np.fft.fft2(self.modified_image[2])
+        shifted_fourier_transformed_image = np.fft.fftshift(fourier_transformed_image)
+        self.original_image_fourier_components = shifted_fourier_transformed_image
         self.modified_image_fourier_components = deepcopy(self.original_image_fourier_components) 
-        
+    
+    
+    def handle_image_size(self , height , width):
+        current_image_height , current_image_width  = self.original_image[2].shape[:2]  
+        if(width == current_image_width and height == current_image_height):
+            return
+        else:
+            self.modified_image[0] = np.arange(1 , height + 1)
+            self.modified_image[1] = np.arange(1 , width + 1)
+            self.modified_image[2] = cv2.resize(self.original_image[2] , (width , height))
+            self.fourier_transform_image()
